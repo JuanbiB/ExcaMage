1cebe6b7aac4b8e9220d5d1f3df4d26a5237267b
JuanbiB <Juanbi@ip-93-226.wireless.oberlin.edu>
2016-04-18 22:46:56 -0400

    Jack and Juanbi flying enemy!
    have fun steve, don’t forget to use lerp!

---
+++ Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingEnemy : MonoBehaviour {
+
+	GameObject player;
+	Rigidbody2D rb;
+	Vector3 temp;
+	int speed;
+	bool moving_towards;
+	float time;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		speed = 3;
+
+		temp = transform.position;
+		moving_towards = true;
+		time = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 direction = player.transform.position - transform.position;
+		direction.Normalize ();
+
+		temp = transform.position + direction * Time.deltaTime * speed;
+
+		if (moving_towards == true) {
+			rb.MovePosition (temp);
+		} else {
+			time += Time.deltaTime;
+		}
+
+		if (time > .8f) {
+			time = 0.0f;
+			moving_towards = true;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Player") {
+			Vector3 dir = player.transform.position - transform.position;
+			dir.Normalize ();
+			rb.AddForce (-dir * 250);
+			moving_towards = false;
+		}
+	}
+}
