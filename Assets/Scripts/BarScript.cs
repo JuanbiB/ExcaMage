6db10f7dbdc9d98105859f31e6dbe7f606685231
n00z <jreynol2@oberlin.edu>
2016-04-23 02:15:53 -0400

    health bar UI working
    The script is basic, but there’s a lot of potential as to where to go.
    Tomorrow morning I’ll build the health animation where it will shrink
    and grow as needed, and then connect it to the player character to test
    while I begin working on the boss script.

---
+++ Assets/Scripts/BarScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+	[SerializeField] private float fillAmount;
+
+	[SerializeField]
+	private Image content;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		handleBar ();
+	
+	}
+
+	private void handleBar(){
+
+		content.fillAmount = fillAmount;
+		
+	}
+}
