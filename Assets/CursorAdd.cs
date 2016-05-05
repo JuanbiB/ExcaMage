d6637b6f15d36350fc2f32d48f8660e7c8a0aab2
JuanbiB <Juanbi@ip-93-226.wireless.oberlin.edu>
2016-05-04 21:38:18 -0400

    u know what it is
    black and yellow, black and yellow

---
+++ Assets/CursorAdd.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorAdd : MonoBehaviour {
+
+	public Texture2D cursor;
+
+	// Use this for initialization
+	void Awake () {
+		Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+	}
+}
